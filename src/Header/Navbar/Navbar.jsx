887434a6c4b65f7895438c759e9f60a244a20cfd
jose luis <dark.gtn@gmail.com>
2022-06-24 15:06:22 -0500

    agregé una foto de prueba

--- src/Header/Navbar/Navbar.jsx
+++ src/Header/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import './Navbar.css';
-import {IoMenu} from 'react-icons/io5';
+import {IoMenu, IoHome, IoImage, IoBookmark} from 'react-icons/io5';
 
 
 const openMenu = () => {
     const navbar =  document.querySelector('nav');
     const button = document.querySelector('.button-navbar');
+    const menu = document.querySelector('.menu__main-container')
+    menu.classList.toggle('menu-active')
     button.classList.toggle('button-navbar-active');
     navbar.classList.toggle('navbar-active');
     
 }
 
-const navbarBackground = [
+/*const navbarBackground = [
     {background: './NuevaCarpeta/20170123_130952.jpg'}
 ]
 
@@ -19,10 +21,10 @@ const navbarBackgroundRandom = (array) => {
     const navbarStyles = document.styleSheets[3].cssRules[2].style;
     console.log(navbarStyles);
     
-    /* navbarStyles[4].cssRules[2].style.backgroundColor = 'blue'; */
+     navbarStyles[4].cssRules[2].style.backgroundColor = 'blue'; 
 }
 
-navbarBackgroundRandom()
+navbarBackgroundRandom()*/
 
 
 const Navbar = () =>{
@@ -31,6 +33,15 @@ const Navbar = () =>{
             <button className="button-navbar" onClick={openMenu}>
                 <IoMenu />
             </button>
+            <div className="menu__main-container">
+                <ul className="menu-container">
+                    <li><IoHome/> <span>Inicio</span> </li>
+                    <li><IoImage/> <span>Galeria</span></li>
+                    <li><IoBookmark/> <span>Reservas</span></li>
+                    <li><IoMenu/> <span>Menú</span></li>
+                </ul>
+            </div>
+            
         </nav>
     )
 }
